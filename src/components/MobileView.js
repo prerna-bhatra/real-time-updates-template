2e92eef366d5a1c3b5b80359ccb56fc55b686158
prernaorangeleague <prerna@orangeleague.in>
2024-06-29 09:14:22 +0530

    subtarct bordrr

--- src/components/MobileView.js
+++ src/components/MobileView.js
@@ -3,8 +3,9 @@ import { Box, Typography, Paper } from '@mui/material';
 
 const MobileView = () => {
   return (
-    <Paper elevation={3} sx={{ width: 300, borderRadius: '40px 40px 0 0', overflow: 'hidden' }}>
-      <Box sx={{ backgroundColor: 'lightgray', height: '150px', position: 'relative' }}>
+    <Box elevation={0} sx={{ padding:'40px', border:'1px solid black', borderBottom:'none', width: 300, borderRadius: '40px 40px 0 0', overflow: 'hidden' }}>
+
+      <Box elevation={0} sx={{ borderLeft: '1px solid black', borderRight: '1px solid black', width: 300, borderRadius: '40px 40px 0 0', overflow: 'hidden', position: 'relative' }}>
         <Box
           sx={{
             position: 'absolute',
@@ -18,15 +19,17 @@ const MobileView = () => {
             borderLeft: '1px solid black',
             borderRight: '1px solid black',
             borderRadius: '0 0 50px 50px',
+            // marginLeft:'100px'
           }}
         />
+        <Box sx={{ padding: 2 }}>
+          <Typography variant="body1" marginTop={10} fontWeight="bold">The Green Yoga</Typography>
+          <Typography variant="body2" color="textSecondary">{new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Typography>
+          <Typography variant="h6" sx={{ marginTop: 1 }}>Hi </Typography>
+        </Box>
       </Box>
-      <Box sx={{ padding: 2 }}>
-        <Typography variant="body1" fontWeight="bold">The Green Yoga</Typography>
-        <Typography variant="body2" color="textSecondary">{new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Typography>
-        <Typography variant="h6" sx={{ marginTop: 1 }}>Hi </Typography>
-      </Box>
-    </Paper>
+
+    </Box>
   );
 };
 
